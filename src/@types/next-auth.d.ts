50617c3bc15aa217f43d7efacb7a0cd11e74cb5d
William Mushkin <william-mush@users.noreply.github.com>
2025-03-28 21:41:13 -0600

    fix: resolve NextAuth type definition conflicts

--- src/@types/next-auth.d.ts
+++ src/@types/next-auth.d.ts
@@ -2,18 +2,26 @@ import { DefaultSession, DefaultUser } from 'next-auth';
 import { JWT as DefaultJWT } from 'next-auth/jwt';
 
 declare module 'next-auth' {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
   interface Session {
     user: {
       id: string;
     } & DefaultSession['user']
   }
 
+  /**
+   * The shape of the user object returned in the OAuth providers' `profile` callback,
+   * or the second parameter of the `session` callback, when using a database.
+   */
   interface User extends DefaultUser {
     id: string;
   }
 }
 
 declare module 'next-auth/jwt' {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
   interface JWT extends DefaultJWT {
     id: string;
   }
